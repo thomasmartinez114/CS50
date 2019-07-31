9433c3cb6087969f512d19a049d2434921be7ede
tmartinez114 <tmartinez114@gmail.com>
2019-07-31 11:45:31 -0400

    implemented Do While loop

--- 01-week/pset2-mario-less.c
+++ 01-week/pset2-mario-less.c
@@ -5,23 +5,11 @@
 
 int main(void)
 {
-    //Prompt user for height of pyramid
-    int height = get_int("Height of pyramid: ");
+    int height;
 
-    //Check if the height isn't < 1 || > 8
-    if (height < 1 || height > 8)
+    // Loop
+    do
     {
-        printf("Height invalid! Needs to be between 1-8");
-    }
-    else
-    {
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                printf("#");
-            }
-            printf("\n");
-        }
-    }
+        height = get_int("Height of pyramid is: ");
+    } while (height == 0 || height < 0 || height > 8);
 }
