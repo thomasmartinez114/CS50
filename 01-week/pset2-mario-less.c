063c1be409c5b8c86205900e148c8a9704f1d37a
tmartinez114 <tmartinez114@gmail.com>
2019-07-31 23:10:01 -0400

    made hashtag show

--- 01-week/pset2-mario-less.c
+++ 01-week/pset2-mario-less.c
@@ -11,7 +11,31 @@ int main(void)
     do
     {
         height = get_int("Height of pyramid is: ");
-    } while (height < 1 || height > 8);
+    } while (height < 0 || height > 8);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+#include <cs50.h>
+#include <stdio.h>
+
+// int get_positive_int(string prompt);
+
+int main(void)
+{
+    int height;
+
+    // Loop
+    do
+    {
+        height = get_int("Height of pyramid is: ");
+    } while (height < 0 || height > 8);
 
     for (int i = 0; i < height; i++)
     {
