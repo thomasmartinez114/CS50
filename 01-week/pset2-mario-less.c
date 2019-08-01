18faf8fab02307d74d9c590ab98dfade6ec1126a
tmartinez114 <tmartinez114@gmail.com>
2019-08-01 18:50:13 -0400

    got the . to display

--- 01-week/pset2-mario-less.c
+++ 01-week/pset2-mario-less.c
@@ -15,30 +15,7 @@ int main(void)
 
     for (int i = 1; i < height + 1; i++)
     {
-        for (int j = 0; j < i; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
-}
-#include <cs50.h>
-#include <stdio.h>
-
-// int get_positive_int(string prompt);
-
-int main(void)
-{
-    int height;
-
-    // Loop
-    do
-    {
-        height = get_int("Height of pyramid is: ");
-    } while (height < 0 || height > 8);
-
-    for (int i = 1; i < height + 1; i++)
-    {
+        printf(".");
         for (int j = 0; j < i; j++)
         {
             printf("#");
