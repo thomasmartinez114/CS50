f61135da492e49ec44005596be9ce1e1c90ff465
tmartinez114 <tmartinez114@gmail.com>
2019-08-06 19:36:19 -0400

    printed out how many quarters it takes

--- 01-week/pset3-cash.c
+++ 01-week/pset3-cash.c
@@ -2,20 +2,40 @@
 #include <cs50.h>
 #include <math.h>
 
+// Get amount in dollars
+// Convert to cents
+// while (quarters can be used)
+//     increase count
+//     decrease amount by a quarter
+// while (dimes can be used)
+//     increase count
+//     decrease amount by a dime
+//  (etc)
+//  print number of coins used
+//  Can use modulo to do this
+//  Example: 50 % 49 = 1
+//      49 goes into 50 once and 1 is remaining
+//   53 % 50 = 3
+
 int main(void)
 {
     float dollars;
 
     do
     {
-        dollars = get_float("How much is your change?: ");
-    } while (dollars <= 0);
-    {
-        // Print out the input into currency format
-        //  printf("The change is $%.2f\n", dollars);
+        dollars = get_float("Dollar amount: $");
+    } while (dollars < 0.0);
 
-        // Convert the input to pennies
-        int cents = round(dollars * 100);
-        printf("The change in Pennies is %i\n", cents);
+    int totalCents = roundf(dollars * 100);
+    printf("Total amount cents = %i\n", totalCents);
+
+    int sum = 0;
+    // Count of Quarters
+    while (25 <= totalCents)
+    {
+        sum++;
+        totalCents -= 25;
     }
+
+    printf("I have %i coin(s)\n", sum);
 }
