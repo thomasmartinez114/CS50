315044b9180452c1bb2bae1d65e5a6362fd5fb0e
tmartinez114 <tmartinez114@gmail.com>
2019-08-14 21:32:42 -0400

    added Command Line Arg. - Success or Usage printf

---
+++ 02-week/pset1-caesar.c
@@ -0,0 +1,21 @@
+// 1 Check one command line arg
+// 2 Go through arg to ensure char are digits
+// 3 Convert arg: String to Int
+// 4 Prompt for plaintext
+// 5 Loop through maintaining case sensitivity (if exist)
+// 6 Print \n
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        printf("Success\n");
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+    }
+}
