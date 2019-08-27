6227a24484453f1251f9d637aa8c525df3510d5a
tmartinez114 <tmartinez114@gmail.com>
2019-08-27 16:50:33 -0400

    completed pset caesar

--- 02-week/pset1-caesar.c
+++ 02-week/pset1-caesar.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
@@ -23,62 +24,67 @@ int main(int argc, string argv[])
     // Pull key - ./caesar [key] from string to int
     int key = atoi(argv[1]);
 
-    // Prompt user for Plaintext that will be encrypted
-    string plain = get_string("Plaintext: ");
-
-    char cipherText[(strlen(plain) + key)];
-    // ex) Tommie length + key
-
-    // Run through each letter ASCII
-    for (int i = 0; i < strlen(plain); i++)
+    // Key needs to be positive
+    if (key < 0)
     {
-        // Check if lowercase --- "a" = 97 -- "z" = 122
-        if (plain[i] >= "a" && plain[i] <= "z")
-        {
-            // Print out between lowercase ASCII
-            printf("%c", (((plain[i] - "a") + key) % 26) + "a");
-        }
-        // Check if uppercase "A" = 65 -- "Z" = 90
-        else if (plain[i] >= "A" && plain[i] <= "Z")
-        {
-            // Print out between uppercase ASCII
-            printf("%c", (((plain[i] - "A") + key) % 26) + "A");
-        }
+        printf("Key requirement is a Positive Integer!\n");
 
-        cipherText[i] = plain[i] + key;
+        return 1;
+    }
+    else
+    {
+        // Prompt user for Plaintext that will be encrypted
+        string plain = get_string("Plaintext: ");
+
+        //     char cipherText[(strlen(plain) + key)];
+        // ex) Tommie length + key
+
+        // Print Ciphered text
+        printf("Ciphertext: ");
+
+        // Run through each letter ASCII
+        for (int i = 0; i < strlen(plain); i++)
+        {
+            if (islower(plain[i]))
+            {
+                // 97 equals "a"
+                printf("%c", (((plain[i] + key) - 97) % 26) + 97);
+            }
+            else if (isupper(plain[i]))
+            {
+                printf("%c", (((plain[i] + key) - 65) % 26) + 65);
+            }
+            else
+            {
+                // Console print
+                printf("%c", plain[i]);
+            }
+        } // End loop
+        printf("\n");
+        return 0;
     }
 }
 
-// #include <cs50.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-
-// int main(int argc, string argv[])
-// {
-//     // Validate argument
-//     if (argc != 2)
+//     // Run through each letter ASCII
+//     for (int i = 0; i < strlen(plain); i++)
 //     {
-//         printf("Usage: ./caesar key \n");
-
-//         // Exit
-//         return 1;
-//     }
-//     else
-//     {
-//         // Converting key to integer
-//         int key = atoi(argv[1]);
-
-//         // Pronpt user for plaintext to cipher
-//         string encrypt = get_string("Plaintext: ");
-
-//         char cipherText[(strlen(encrypt) + key)];
-//         // ex) tommie length + key
-
-//         for (int i = 0; i < strlen(encrypt); i++)
+//         // Check if lowercase --- "a" = 97 -- "z" = 122
+//         if (plain[i] >= "a" && plain[i] <= "z")
 //         {
-//             cipherText[i] = encrypt[i] + key;
+//             // Print out between lowercase ASCII
+//             printf("%c", (((plain[i] - "a") + key) % 26) + "a");
 //         }
-//         printf("Ciphertext: %s\n", cipherText);
+//         // Check if uppercase "A" = 65 -- "Z" = 90
+//         else if (plain[i] >= "A" && plain[i] <= "Z")
+//         {
+//             // Print out between uppercase ASCII
+//             printf("%c", (((plain[i] - "A") + key) % 26) + "A");
+//         }
+//         else
+//         {
+//             printf("%c", plain[i]);
+//         }
 //     }
+//     printf("\n");
+//     return 0;
 // }
