65a5e7b34e93f7beb142a26de11c91e25bc9026c
tmartinez114 <tmartinez114@gmail.com>
2019-11-15 16:18:27 -0500

    printf() the correct amount of # in the first column

--- pset-2nd-attempt/01-week/pset1-2-mario-less.c
+++ pset-2nd-attempt/01-week/pset1-2-mario-less.c
@@ -2,27 +2,30 @@
 // If height is less than 1 or greater than 8 (or not an int), go back to step 1
 // Number will create the hashtags
 //   ex) 2 inserted will create 1 tag, 2 tags
+//   https://www.youtube.com/watch?v=Ri4z32bIGNM
+
 
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int height;
+    // int height;
 
-    //     While Loop
-    do
-    {
-        height = get_int("Enter Height of Pyramid between 1 - 8: ");
-        // printf("Height is %i \n", height);
-    } while (height < 1 || height > 8);
+//     While Loop
+//     do
+//     {
+//       height = get_int("Enter Height of Pyramid between 1 - 8: ");
+//       // printf("Height is %i \n", height);
+//     } while (height < 1 || height > 8);
 
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < height; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
+    // remove this after testing statically
+    int height = 5;
+
+      for (int i = 1; i <= height; i++)
+      {
+          printf("#");
+          printf("\n");
+      }
 }
+
