ecfc211f92c7430c0cf7684b5a151c12ca2a8d21
tmartinez114 <tmartinez114@gmail.com>
2019-11-16 19:56:38 -0500

    got the change to display in cents

--- pset-2nd-attempt/01-week/pset1-3-cash.c
+++ pset-2nd-attempt/01-week/pset1-3-cash.c
@@ -5,12 +5,17 @@
 int main(void)
 {
 
-float change;
+float dollars;
 
 do
 {
-    change = get_float("How much is your change: \n");
-    printf("Change is %f\n", change);
+    dollars = get_float("How much is your change: \n");
+    // printf("Change is %f\n", dollars);
 }
-while (change < 0);
+while (dollars < 0);
+
+int cents = round(dollars * 100);
+printf("Your change in cents is: %i\n", cents);
 }
+
+
