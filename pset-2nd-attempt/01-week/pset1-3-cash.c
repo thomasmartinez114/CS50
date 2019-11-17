c7050a1d63977186bee1e74987dfb8e30b315668
tmartinez114 <tmartinez114@gmail.com>
2019-11-16 20:05:55 -0500

    added psuedocode and placeholder for coins used

--- pset-2nd-attempt/01-week/pset1-3-cash.c
+++ pset-2nd-attempt/01-week/pset1-3-cash.c
@@ -2,20 +2,30 @@
 #include <cs50.h>
 #include <math.h>
 
+// Get amount in dollars
+// Convert to cents
+// While (quarters can be used)
+//     increase count
+//     decrease amount by quarter
+// While (dimes can be used)
+//     increase count
+//     decrease amount by dime
+//  (etc).
+//  Print number of coins used
+
 int main(void)
 {
 
-float dollars;
+    int coins; // Placeholder for coins
+    float dollars;
+    do
+    {
+        dollars = get_float("How much is your change: \n");
+        // printf("Change is %f\n", dollars);
+    } while (dollars < 0);
 
-do
-{
-    dollars = get_float("How much is your change: \n");
-    // printf("Change is %f\n", dollars);
+    int cents = round(dollars * 100);
+    printf("Your change in cents is: %i\n", cents);
+
+    // printf("I have %i coin(s)\n", coins);
 }
-while (dollars < 0);
-
-int cents = round(dollars * 100);
-printf("Your change in cents is: %i\n", cents);
-}
-
-
