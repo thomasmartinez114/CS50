c4360875fe9d71dca4da84967593264075267ab3
tmartinez114 <tmartinez114@gmail.com>
2019-11-25 19:40:13 -0500

    added pseudo code

---
+++ pset-2nd-attempt/02-week/pset2-1-caesar.c
@@ -0,0 +1,9 @@
+// Check program was run with command line
+// Iterate over the arugment to make sure all char are digits
+// Convert that command line argument from a string to an int
+// Prompt user for plaintext
+// Iterate over each char of the plaintext:
+//     1) if it is uppercase letter, rotate it, saving the case, then print out rotated char
+//     2) if it is lowercase letter, rotate it, saving the case, then print out the rotated char
+//     3) if it is neither, print out the char as is
+// Print a newline
