dd9f574f9b407eb9630ef8db805b8c1118f2eaee
tmartinez114 <tmartinez114@gmail.com>
2019-12-05 18:03:50 -0500

    attempting credit

--- pset-2nd-attempt/01-week/pset1-3-credit.c
+++ pset-2nd-attempt/01-week/pset1-3-credit.c
@@ -5,11 +5,22 @@
 // Prompt for credit card number
 // Return whether its AMEX, VISA, MASTERCARD or INVALID
 // get_long not get_int
+// Card number beginnings:
+// AMEX: 34 or 37
+// MASTERCARD: 51 - 55
+// VISA: 4
 
 int main(void)
 {
     long cardNumber;
 
     cardNumber = get_long("Enter your card number: ");
-    printf("Card number is: %ld\n", cardNumber);
+    // printf("Card number is: %ld\n", cardNumber);
+    int card2 = scanf("%2ld", &cardNumber);
+    printf(card2);
+
+    //     for (var cardDigits = 0; cardDigits < cardNumber.length; cardDigits++)
+    //     {
+    //         if (cardNumber)
+    //     }
 }
